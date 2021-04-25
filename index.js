ac0bd851ca77cde82ecbd7dc09a0313f904a7d18
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-04-25 21:20:09 +0200

    added some routes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //importing dependencies
-const cors = require("cors");
-const express = require("express");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const { v4: uuidv4, v4 } = require("uuid");
+const cors = require('cors');
+const express = require('express');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const { v4: uuidv4, v4 } = require('uuid');
 //initializing app
 const app = express();
 
@@ -11,8 +11,32 @@ app.use(express.json());
 app.use(cors());
 
 //routes
-app.get("/", (req, res) => {
-  res.send("IT WORKS");
+app.get('/', (req, res) => {
+  res.send('IT WORKS');
+});
+
+app.get('/payment/create', async (req, res) => {
+  try {
+    //getting info from the client
+    const { amount, shipping } = req.body;
+    //sending payment intent
+    const paymentIntent = await stripe.paymentIntent.create({
+      shipping,
+      amount,
+      currency: 'usd',
+    });
+    //resolving
+    res.status(200).send(paymentIntent.client_secret);
+  } catch (err) {
+    res.status(500).json({
+      statusCode: 500,
+      message: err.message,
+    });
+  }
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send('404, not found');
 });
 
 //listen
