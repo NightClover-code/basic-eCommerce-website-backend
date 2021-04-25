23600010fbd4ef38856fcbef8b59da1490bbee8a
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-04-25 16:22:01 +0200

    adding eslint parser

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //importing dependencies
-const cors = require('cors');
-const express = require('express');
-const stripe = require('stripe')('');
-const uuid = require('uuid/dist/v4');
+const cors = require("cors");
+const express = require("express");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const { v4: uuidv4, v4 } = require("uuid");
 //initializing app
 const app = express();
 
@@ -11,10 +11,12 @@ app.use(express.json());
 app.use(cors());
 
 //routes
-app.get('/', (req, res) => {
-  res.send('It works yep!');
+app.get("/", (req, res) => {
+  res.send("IT WORKS");
 });
 
 //listen
-const port = 8000;
-app.listen(port, () => console.log(`listenning at port ${port}`));
+const port = 8282;
+app.listen(port, () =>
+  console.log(`listening at port ${port}`, `visit http://localhost:${port}`)
+);
